9aaba67db71fab9699f2cc86a6dd753fcd95ee4b
Garurab-Subedi <gauaravsubedi299@gmail.com>
2025-07-12 16:11:52 +0545

    login has done

--- Gaurav-ecom/src/Pages/LoginSignUp.jsx
+++ Gaurav-ecom/src/Pages/LoginSignUp.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../CSS/LoginSignUp.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Enter your email address' />
+          <input type="password" placeholder='Enter Your password' />
+        </div>
+        <button className='loginsignup-btn'>Sign Up</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- Gaurav-ecom/src/Pages/Product.jsx
+++ Gaurav-ecom/src/Pages/Product.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {prodcutId} = useParams();
+  const product = all_product.find((e) => e.id === prodcutId)
   return (
     <div>Product</div>
   )

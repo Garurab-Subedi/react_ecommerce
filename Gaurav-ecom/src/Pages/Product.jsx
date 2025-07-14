b9d413f232e64a4e7c49663ca74fed6dc835ec4e
Garurab-Subedi <gauaravsubedi299@gmail.com>
2025-07-14 20:38:52 +0545

    add to cart function done

--- Gaurav-ecom/src/Pages/Product.jsx
+++ Gaurav-ecom/src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext'
 import Bredcrums from '../Components/Bredcrums/Bredcrums';
 import { useParams } from 'react-router-dom'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import Description from '../Components/Description/Description';
 const Product = () => {
   const {all_product} = useContext(ShopContext);
   const {productId} = useParams();
@@ -11,6 +12,7 @@ const Product = () => {
     <div>
       <Bredcrums product = {product} />
       <ProductDisplay product = {product} />
+      <Description />
     </div>
   )
 }
